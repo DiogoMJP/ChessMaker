80b8b4886b470d65ba1cf7fad03cb9cde2bbe092
Afonso Freitas <afonso.duarte.freitas@tecnico.ulisboa.pt>
2024-02-29 17:07:17 +0000

    feat(Pilot): First Commit with something c:

---
+++ App/Chess.java
@@ -0,0 +1,7 @@
+package App;
+
+public class Chess {
+    public static void main(String[] args) {
+        System.out.println("THIS WILL BE THE CHESS GAME!");
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ChessMaker
+
+Run in App/ javac Chess.java
+After that com back to the root and execute java App/Chess
+
